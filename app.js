21a66f19f60d105998717a09d095160ee4680ba6
Chris Waters <chris@innstyle.co.uk>
2019-05-07 23:52:00 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,54 @@
+"use strict";
+
+let form = document.querySelector('form');
+let question = document.querySelector('[data-text="question"]');
+let result = document.querySelector('[data-text="result"]');
+let answer = form.querySelector('[data-input="answer"]');
+let multiplicand = form.querySelector('[data-value="multiplicand"]');
+let multiplier = form.querySelector('[data-value="multiplier"]');
+
+function randomNumber() {
+  return Math.floor(Math.random() * 12);
+}
+
+function resetAnswer() {
+  answer.value = null;
+  result.innerHTML = null;
+}
+
+function showQuestion() {
+  question.innerHTML = `${multiplicand.value} x ${multiplier.value}`;
+}
+
+function generateQuestion() {
+  multiplicand.value = randomNumber();
+  multiplier.value = randomNumber();
+  resetAnswer();
+  showQuestion();
+}
+
+function isCorrect() {
+  return Number(answer.value) == (multiplicand.value * multiplier.value);
+};
+
+function resultMessage() {
+  return isCorrect() ? 'Correct!' : 'Nope!';
+}
+
+function showResult() {
+  result.innerHTML = resultMessage();
+}
+
+function nextStep() {
+  return isCorrect() ? generateQuestion() : resetAnswer();
+}
+
+function processResult() {
+  showResult();
+  setTimeout(() => nextStep(), 440);
+  return false;
+}
+
+form.onsubmit = processResult;
+
+generateQuestion();
